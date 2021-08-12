d6bb2486508b0f5e20cd82c4fd540fc909bcbc28
mimzivvimzi <mdeniselau@gmail.com>
2021-08-13 08:00:56 +0900

    Find product name (title) and price

--- webscraper.go
+++ webscraper.go
@@ -9,53 +9,66 @@ import (
 	"github.com/gocolly/colly"
 )
 
-type Listing struct {
+type Product struct {
 	// ID int `json:"id"`
-	Description string `json:"description"`
-	Location string `json:"location"`
+	Title string `json:"title"`
+	Price string `json:"price"`
 	// Link string `json:"link"`
 }
 
 func main() {
-	allListings := make([]Listing, 0)
+	allProducts := make([]Product, 0)
 
 	c := colly.NewCollector()
 
-	c.OnHTML(".listing-item.listing-title", func(e *colly.HTMLElement) {
-		// fmt.Println(e.ChildText("span"))
-		listingDesc := e.ChildText(".text-semi-strong")
-		listingLoc := e.ChildText("span")
-		// link := e.Attr("href")
-		
-
-		listing := Listing{
-			Description: listingDesc,
-			Location: listingLoc,
-			// Link: link,
-		}
-
-		allListings = append(allListings, listing)
-		// test := e.Request.Visit(e.Attr("href"))
-		// fmt.Println(test)
-	})
-
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
-		if strings.Index(link, "/en/rent/view/") != -1 {
-			fmt.Println(link)
-			// c.Visit(link)
+		if !strings.HasPrefix(link, "/product/") {
+			return
 		}
-		// fmt.Println(link)
+		e.Request.Visit(link)
 	})
+	
+	c.OnHTML(".col-sm-4.col-lg-4.col-md-4", func(e *colly.HTMLElement) {
+	// c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+
+		title := e.ChildText(".title")
+		price := e.ChildText(".pull-right.price")
+
+		// test := e.Request.Visit(e.Attr("href"))
+		// fmt.Println(link)
+
+		product := Product{
+			Title: title,
+			Price: price,
+		}
+	
+		allProducts = append(allProducts, product)
+	})
+
+
+
+
+	// c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+	// 	link = e.Attr("href")
+	// 	// if strings.Index(link, "/en/rent/view/") != -1 {
+	// 	// 	fmt.Println(link)
+	// 	// 	// c.Visit(link)
+	// 	// }
+	// 	fmt.Println(link)
+	// })
+
+
+
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
 	})
 
-	c.Visit("https://apartments.gaijinpot.com/en/rent/listing?page=3")
+	c.Visit("https://webscraper.io/test-sites/e-commerce/static/computers/laptops")
 
 	enc := json.NewEncoder(os.Stdout)
 	enc.SetIndent("", "")
-	enc.Encode(allListings)
+	enc.Encode(allProducts)
 	// log.Println(c)
 }
